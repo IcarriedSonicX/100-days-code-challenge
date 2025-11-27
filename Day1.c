3593f97f6e1503680ce2972c6a1de9c4cae80770
IcarriedSonicX <RISHAV.26124@stu.upes.ac.in>
2025-11-27 16:55:07 +0530

    Create Day1.c

---
+++ Day1.c
@@ -0,0 +1,43 @@
+@@ -1,4 +1,18 @@
+//question 1
+//Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+#include <stdio.h>
+void main() {
+    int a,b;
+@@ -8,7 +22,21 @@ void main() {
+    printf("sum = %d",sum);
+}
+
+//question 2
+//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+/*
+Sample Test Cases:
+Input 1:
+10 2
+Output 1:
+Sum=12, Diff=8, Product=20, Quotient=5
+
+Input 2:
+7 3
+Output 2:
+Sum=10, Diff=4, Product=21, Quotient=2
+
+*/
+#include <stdio.h>
+void main() {
+    int a,b;
